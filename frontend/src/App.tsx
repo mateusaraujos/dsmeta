9f70bf76970ecdf5e20d955e3b38817d2817db7e
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-11-19 19:19:44 -0300

    NotificationButton component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
+  // Um componente React não pode exportar mais de 1 tag.
+  // Quando tiver mais que 1 tag, colocar todos dentro do Pai <> </>.
   return (
-    <h1>Hello, World!</h1>
-  )
+    <>
+      <h1>Hello, World!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
