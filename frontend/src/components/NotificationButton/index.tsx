b705986d650339cdcc8d676d0fe494fd68c27d84
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-11-21 01:59:41 -0300

    Notification sent successfully

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,25 @@
 // Importando imagem do ícone para poder usá-la.
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
 
-function NotificationButton() {
+// Colocar os dados que o componente NotificationButton vai receber.
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((responde) => {
+    console.log("Notification sent successfully!");
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
+  // Quando o botão for clicado, vai rodar a função handleClick(saleId);
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notify" />
     </div>
   );
