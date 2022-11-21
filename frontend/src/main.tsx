918c007abd0950098587df19383c4f1145075fcb
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-11-21 02:22:55 -0300

    window.React = React

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Para evitar bug quando tentar enviar SMS na aplicação implantada no Netlify.
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
