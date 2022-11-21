b92c0577806b099c283f9180fa763b8fe48e6ad5
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-11-21 00:09:29 -0300

    heroku SMS OK

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dsmeta.services;
 
-import java.text.DecimalFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
